772ddf7bf9eff46308de49ae5f9b491d6c90439f
ARP <andrewarp06@gmail.com>
2026-05-05 18:28:11 +0200

    Actividad 6
    Ya es una tradición no hacer el 1 
    Normal que no me vaya bien la vida

---
+++ AC6.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actividades.ud10__colecciones;
+import java.util.* ;
+
+/**
+ *
+ * @author PC GAMING
+ */
+public class AC6 {
+        public static void main(String[] args) {
+            Collection <Integer> numero = new ArrayList <> () ;
+            Random random = new Random() ;
+            
+            
+            for(int i = 0 ; i < 20 ; i++){
+                numero.add(random.nextInt(100) + 1);
+            }
+      System.out.println("NUMEROS" + numero) ;
+        
+         Integer[] numero2 = numero.toArray(new Integer[0]) ;
+        Arrays.sort(numero2);
+         System.out.println(Arrays.toString(numero2));
+        
+         Collection <Integer> numero3 = new ArrayList<> (Arrays.asList(numero2)) ;
+         
+               System.out.println("NUMEROS PARTE 3" + numero3) ;
+          
+               
+             Arrays.sort(numero2, Collections.reverseOrder());
+                     System.out.println(Arrays.toString(numero2));
+        } 
+}
