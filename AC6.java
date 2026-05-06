d7ec91efb2236b901c4600a78e229fc4b23b4d8a
ARP <andrewarp06@gmail.com>
2026-05-06 09:18:10 +0200

    Add files via upload

--- AC6.java
+++ AC6.java
@@ -2,30 +2,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package actividades.mavenproject1;
+package actividades.ud10__colecciones;
 import java.util.* ;
+
 /**
  *
- * @author ALUMNO
+ * @author PC GAMING
  */
-public class AC7 {
-     public static void main(String[] args) {
-        List<Integer> lista1 = new ArrayList<>(Arrays.asList(10 , 20 , 30 , 40));
-        List<Integer> lista2 = new ArrayList<>(Arrays.asList(99 , 99));
-        List<Integer> lista3 = new ArrayList<>(Arrays.asList(5));
+public class AC6{
+        public static void main(String[] args) {
+            Collection <Integer> numero = new ArrayList <> () ;
+            Random random = new Random() ;
+            
+            
+            for(int i = 0 ; i < 20 ; i++){
+                numero.add(random.nextInt(100) + 1);
+            }
+      System.out.println("NUMEROS" + numero) ;
         
-        System.out.println("Apartado1: " + lista1);
-        System.out.println("Apartado2: " + lista2);
-        lista1.addAll(0, lista3);
-        System.out.println("Apartado3: " + lista1);
-        lista1.addAll(2, lista2);
-        System.out.println("Apartado4: " + lista1);       
-        System.out.println("Apartado5: " + lista1.get(4));    
-        int sustituido = lista1.set(lista1.size()-1 , 500) ;
-        System.out.println("Apartado6 : " + lista1 + " guardado en la variable sustituido");
-        lista1.remove(1) ;
-        System.out.println("Apartado7 " + lista1 );
-        lista1.remove(lista1.indexOf(99));
-        System.out.println("Apartado8 " + lista1 );
-     }
+         Integer[] numero2 = numero.toArray(new Integer[0]) ;
+        Arrays.sort(numero2);
+         System.out.println(Arrays.toString(numero2));
+        
+         Collection <Integer> numero3 = new ArrayList<> (Arrays.asList(numero2)) ;
+         
+               System.out.println("NUMEROS PARTE 3" + numero3) ;
+          
+               
+             Arrays.sort(numero2, Collections.reverseOrder());
+                     System.out.println(Arrays.toString(numero2));
+        } 
 }
